8bab47ab5fa10598de1ae5e6e180bbc1885eb3b3
Diego Magdaleno <diegommagdaleno@gmail.com>
2018-09-24 23:00:08 -0300

    app initial commit

---
+++ app/src/main/java/com/dmagdaleno/viagens/ListaPacotesActivity.kt
@@ -0,0 +1,12 @@
+package com.dmagdaleno.viagens
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ListaPacotesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_pacotes)
+    }
+}
