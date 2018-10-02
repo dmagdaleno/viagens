9e073aca3bbd852aab856e0415758c64c171620c
Diego Magdaleno <diegommagdaleno@gmail.com>
2018-10-01 22:55:33 -0300

    função para retornar bigdecimal formatado

---
+++ app/src/main/java/com/dmagdaleno/viagens/extensions/BigDecimalExtension.kt
@@ -0,0 +1,16 @@
+package com.dmagdaleno.viagens.extensions
+
+import java.math.BigDecimal
+import java.text.DecimalFormat
+import java.util.Locale
+
+fun BigDecimal.formatado(): String {
+    return DecimalFormat.getCurrencyInstance(Locale("pt", "BR"))
+            .format(this)
+            .replace("R$", "R$ ")
+            .replace("-R$ ", "R$ -")
+}
+
+fun BigDecimal.positivo(): Boolean {
+    return this >= BigDecimal.ZERO
+}
