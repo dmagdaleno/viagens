a8525386ebc98bb8900df0ccaf5ccea164aefa0d
Diego Magdaleno <diegommagdaleno@gmail.com>
2018-10-01 22:53:14 -0300

    refatoração de pacotes

--- app/src/main/java/com/dmagdaleno/viagens/ui/activity/ListaPacotesActivity.kt
+++ app/src/main/java/com/dmagdaleno/viagens/ui/activity/ListaPacotesActivity.kt
@@ -2,11 +2,15 @@ package com.dmagdaleno.viagens
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.dmagdaleno.viagens.ui.adapter.ListaPacotesAdapter
+import kotlinx.android.synthetic.main.activity_lista_pacotes.*
 
 class ListaPacotesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_pacotes)
+
+        lista_pacotes_list.adapter = ListaPacotesAdapter()
     }
 }
