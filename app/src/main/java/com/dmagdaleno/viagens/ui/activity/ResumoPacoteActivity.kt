783b0d42b528ff3d6b076b130aaaeb0dd879d1e9
Diego Magdaleno <diegommagdaleno@gmail.com>
2018-10-24 22:28:53 -0300

    activity que representa o resumo do pacote

---
+++ app/src/main/java/com/dmagdaleno/viagens/ui/activity/ResumoPacoteActivity.kt
@@ -0,0 +1,13 @@
+package com.dmagdaleno.viagens.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.dmagdaleno.viagens.R
+
+class ResumoPacoteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resumo_pacote)
+    }
+}
