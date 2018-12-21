fbe85780156a0b788091c51db9bef44745ceffc4
Diego Magdaleno <diegommagdaleno@gmail.com>
2018-12-21 13:59:06 -0200

    refatora função que formata as datas de ida e volta

--- app/src/main/java/com/dmagdaleno/viagens/ui/activity/ResumoPacoteActivity.kt
+++ app/src/main/java/com/dmagdaleno/viagens/ui/activity/ResumoPacoteActivity.kt
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import com.dmagdaleno.viagens.R
 import com.dmagdaleno.viagens.functions.extensions.formatado
 import com.dmagdaleno.viagens.functions.extensions.getDrawableResource
+import com.dmagdaleno.viagens.functions.getDataIdaVoltaFormatada
 import com.dmagdaleno.viagens.functions.getDiasText
 import com.dmagdaleno.viagens.model.Pacote
 import kotlinx.android.synthetic.main.activity_resumo_pacote.*
@@ -16,11 +17,15 @@ class ResumoPacoteActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resumo_pacote)
 
-        val pacote = Pacote("sao_paulo_sp", "São Paulo", 2, BigDecimal(243.99))
+        val pacote = Pacote("sao_paulo_sp", "São Paulo", 10, BigDecimal(243.99))
 
         resumo_pacote_imagem.setImageDrawable(getDrawableResource(pacote.imagem))
         resumo_pacote_local.text = pacote.local
         resumo_pacote_dias.text = getDiasText(pacote.dias)
         resumo_pacote_preco.text = pacote.preco.formatado()
+
+        val data = getDataIdaVoltaFormatada(pacote)
+
+        resumo_pacote_data.text = data
     }
 }
