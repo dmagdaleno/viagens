51b7450f508d299484fe82843a74082ed2acdeb0
Diego Magdaleno <diegommagdaleno@gmail.com>
2018-12-21 11:05:57 -0200

    formatando as informações da tela de resumo

--- app/src/main/java/com/dmagdaleno/viagens/ui/activity/ResumoPacoteActivity.kt
+++ app/src/main/java/com/dmagdaleno/viagens/ui/activity/ResumoPacoteActivity.kt
@@ -3,7 +3,9 @@ package com.dmagdaleno.viagens.ui.activity
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.dmagdaleno.viagens.R
-import com.dmagdaleno.viagens.extensions.getDrawableResource
+import com.dmagdaleno.viagens.functions.extensions.formatado
+import com.dmagdaleno.viagens.functions.extensions.getDrawableResource
+import com.dmagdaleno.viagens.functions.getDiasText
 import com.dmagdaleno.viagens.model.Pacote
 import kotlinx.android.synthetic.main.activity_resumo_pacote.*
 import java.math.BigDecimal
@@ -18,7 +20,7 @@ class ResumoPacoteActivity : AppCompatActivity() {
 
         resumo_pacote_imagem.setImageDrawable(getDrawableResource(pacote.imagem))
         resumo_pacote_local.text = pacote.local
-        resumo_pacote_dias.text = pacote.dias.toString()
-        resumo_pacote_preco.text = pacote.preco.toString()
+        resumo_pacote_dias.text = getDiasText(pacote.dias)
+        resumo_pacote_preco.text = pacote.preco.formatado()
     }
 }
