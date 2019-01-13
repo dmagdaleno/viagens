f5d99df52b29012d4ec0070e51d1f628014ba48f
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-01-13 19:18:03 -0200

    muda Pacote para Parcelable

--- app/src/main/java/com/dmagdaleno/viagens/model/Pacote.kt
+++ app/src/main/java/com/dmagdaleno/viagens/model/Pacote.kt
@@ -1,10 +1,37 @@
 package com.dmagdaleno.viagens.model
 
-import java.io.Serializable
+import android.os.Parcel
+import android.os.Parcelable
 
 data class Pacote(
         val imagem: String = "indefinido",
         val local: String = "indefinido",
         val dias: Int = 0,
         val preco: Double = 0.0
-): Serializable
+): Parcelable {
+
+    constructor(parcel: Parcel) : this(
+            imagem = parcel.readString(),
+            local = parcel.readString(),
+            dias = parcel.readInt(),
+            preco = parcel.readDouble())
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(imagem)
+        parcel.writeString(local)
+        parcel.writeInt(dias)
+        parcel.writeDouble(preco)
+    }
+
+    override fun describeContents() = 0
+
+    companion object CREATOR : Parcelable.Creator<Pacote> {
+        override fun createFromParcel(parcel: Parcel): Pacote {
+            return Pacote(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Pacote?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
