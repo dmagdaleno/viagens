dafe461affb45ff7e7625101a5e8013605664f30
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-01-13 19:43:35 -0200

    removendo todas as activities da stack e retornando para a inicial ao clicar em voltar

--- app/src/main/java/com/dmagdaleno/viagens/ui/activity/ResumoCompraActivity.kt
+++ app/src/main/java/com/dmagdaleno/viagens/ui/activity/ResumoCompraActivity.kt
@@ -1,14 +1,15 @@
 package com.dmagdaleno.viagens.ui.activity
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import com.dmagdaleno.viagens.R
+import com.dmagdaleno.viagens.constantes.App
 import com.dmagdaleno.viagens.functions.extensions.formatado
 import com.dmagdaleno.viagens.functions.extensions.getDrawableResource
 import com.dmagdaleno.viagens.functions.extraiPeriodo
 import com.dmagdaleno.viagens.model.Pacote
 import kotlinx.android.synthetic.main.activity_resumo_compra.*
-import java.math.BigDecimal
 
 class ResumoCompraActivity : AppCompatActivity() {
 
@@ -16,14 +17,22 @@ class ResumoCompraActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resumo_compra)
 
-        if(intent.hasExtra("pacote")){
+        title = App.TITULO
 
-            val pacote = intent.extras.getParcelable("pacote") as Pacote
+        if(intent.hasExtra(App.PACOTE)){
+
+            val pacote = intent.extras.getParcelable(App.PACOTE) as Pacote
 
             resumo_compra_foto_local.setImageDrawable(getDrawableResource(pacote.imagem))
             resumo_compra_local.text = pacote.local
             resumo_compra_periodo.text = extraiPeriodo(pacote.dias)
             resumo_compra_preco.text = pacote.preco.formatado()
         }
+    }
+
+    override fun onBackPressed() {
+        val i = Intent(this, ListaPacotesActivity::class.java)
+        i.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(i)
     }
 }
