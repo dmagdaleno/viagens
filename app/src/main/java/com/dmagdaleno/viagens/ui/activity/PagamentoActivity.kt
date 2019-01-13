9193c1ed9c218a641d9f8cab61498e6a6cec29dd
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-01-13 14:09:50 -0200

    bind do preço do pacote para testes de layout durante a execução

--- app/src/main/java/com/dmagdaleno/viagens/ui/activity/PagamentoActivity.kt
+++ app/src/main/java/com/dmagdaleno/viagens/ui/activity/PagamentoActivity.kt
@@ -3,11 +3,19 @@ package com.dmagdaleno.viagens.ui.activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.dmagdaleno.viagens.R
+import com.dmagdaleno.viagens.functions.extensions.formatado
+import com.dmagdaleno.viagens.model.Pacote
+import kotlinx.android.synthetic.main.activity_pagamento.*
+import java.math.BigDecimal
 
 class PagamentoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pagamento)
+
+        val pacote = Pacote("sao_paulo_sp", "São Paulo", 10, BigDecimal(243.99))
+
+        pagamento_valor.text = pacote.preco.formatado()
     }
 }
